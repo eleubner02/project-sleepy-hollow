f67653fb56f0da2664fb04b937362ffdf2da0387
Eric J. Leubner Jr. <eleubner02@gmail.com>
2026-05-25 12:21:51 -0500

    adjustment from python 3.9 to 3.14 to use gridstatus

--- sleepy_hollow.py
+++ sleepy_hollow.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import gridstatus # for data ingestion
-import scikit-learn as sklearn # for logistic regression
+import sklearn # for logistic regression
 import statsmodels as sm # for ARIMA
 import smtplib # for email routing
-
